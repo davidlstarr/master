40d5394261b944e1c02f5e931b968f3b8939b68a
davidlstarr <davidlstarr@hotmail.com>
2017-07-09 13:15:30 -0400

    setting up project on windows

--- bottom_scripts.php
+++ bottom_scripts.php
@@ -12,3 +12,8 @@
 
 <!--Template functions-->
 <script src="_js/functions.js"></script>
+
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.12'><\/script>".replace("HOST", location.hostname));
+    //]]></script>
+
